f501690611b649daec8044462a9746ac9413d788
fandsimple <fand1024@163.com>
2020-03-03 23:18:25 +0800

    update

--- alg/__init__.py
+++ alg/__init__.py
@@ -3,3 +3,5 @@
 
 
 from .sort import *
+from .recursion import *
+from .query import *
--- data_structure/__init__.py
+++ data_structure/__init__.py
@@ -4,5 +4,8 @@
 from .singleLinkList import *
 from .queue import *
 from .stack import *
+from .heap import *
+from .hashTable import *
+from .skipList import *
 
 
